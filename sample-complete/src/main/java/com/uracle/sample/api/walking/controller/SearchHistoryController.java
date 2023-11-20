2e61c0c63f0b3c8903ef4e6d701b8f36f3b2ec6e
ara <ara020303@gmail.com>
2023-11-20 10:34:52 +0900

    1118

--- sample-complete/src/main/java/com/uracle/sample/api/walking/controller/SearchHistoryController.java
+++ sample-complete/src/main/java/com/uracle/sample/api/walking/controller/SearchHistoryController.java
@@ -20,6 +20,7 @@ import static com.uracle.sample.support.MspUtil.makeResult;
 @Slf4j
 @RestController
 @RequestMapping("/histo")
+@CrossOrigin(origins = "http://localhost:5500")
 public class SearchHistoryController {
 
     @Autowired
