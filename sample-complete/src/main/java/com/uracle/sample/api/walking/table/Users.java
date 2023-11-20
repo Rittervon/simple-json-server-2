56fc46b450e99f7cc353a88d188cb8ede9a82124
ara <ara020303@gmail.com>
2023-11-20 17:53:04 +0900

    1120-last

--- sample-complete/src/main/java/com/uracle/sample/api/walking/table/Users.java
+++ sample-complete/src/main/java/com/uracle/sample/api/walking/table/Users.java
@@ -13,5 +13,6 @@ import lombok.ToString;
 public class Users {
     private String phone_number;
     private String name_or_id;
+    private int user_char_id;
     private LocalDateTime registration_date;
 }
