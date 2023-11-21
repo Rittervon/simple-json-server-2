6066a8f71fe7b4db924e2f0a3051fcbd951471de
hsm3631 <hsm3631@gmail.com>
2023-11-21 10:16:28 +0900

    .

---
+++ sample-complete (3)/src/main/java/com/uracle/sample/api/walking/table/Users.java
@@ -0,0 +1,17 @@
+package com.uracle.sample.api.walking.table;
+
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+
+public class Users {
+    private String phone_number;
+    private String name_or_id;
+    private LocalDateTime registration_date;
+}
